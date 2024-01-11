05873b0283d5467dcb2eadaa3da1752e69e0f8da
Andreas Gruber <andreas.gruber1@iu-study.org>
2024-01-11 21:35:08 +0100

    Navbar mit ReactBootstrap innerhalb App() neu implementiert

--- src/App.jsx
+++ src/App.jsx
@@ -1,36 +1,35 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
-import 'bootstrap'
+import Container from 'react-bootstrap/Container';
+import Nav from 'react-bootstrap/Nav';
+import Navbar from 'react-bootstrap/Navbar';
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+
+    <Navbar sticky="top" expand="lg" className="bg-body-secondary shadow-sm">
+      <Container fluid>
+        <Navbar.Brand href="/index.html">
+          <img 
+            src="/public/images/20240109_Martin.png" 
+            alt="Logo" 
+            className="d-inline-block align-top me-1"
+            height= "32"
+            width= "32"
+          ></img>
+          <strong>CaringCloak</strong>
+        </Navbar.Brand>
+        <Navbar.Toggle aria-controls="basic-navbar-nav" />
+        <Navbar.Collapse id="basic-navbar-nav">
+          <Nav className="me-auto">
+            <Nav.Link href="/index.html">Startseite</Nav.Link>
+            <Nav.Link href="/public/pages/registrierung.html">Registrierung</Nav.Link>
+            <Nav.Link href="/public/pages/faq.html">FAQ</Nav.Link>
+            <Nav.Link href="/public/pages/kontakt.html">Kontakt</Nav.Link>
+          </Nav>
+        </Navbar.Collapse>
+      </Container>
+    </Navbar>
+
+  );
 }
 
-export default App
+export default App;
