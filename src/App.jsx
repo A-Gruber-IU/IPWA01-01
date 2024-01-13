7ec14207836fdec5a7e8ed00925c45cb4cf4be76
Andreas Gruber <andreas.gruber1@iu-study.org>
2024-01-13 20:37:51 +0100

    Finalisierung von Footer und Navbar (ohne React); Erstellung der "Füllseiten" AGB, Datenschutz, Impressum, Layout für Registrierung

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,20 @@
-import Container from 'react-bootstrap/Container';
-import Nav from 'react-bootstrap/Nav';
-import Navbar from 'react-bootstrap/Navbar';
+import Logistikauswahl from './Logistikauswahl.jsx';
+import Header from './Header.jsx';
+import { Col, Row } from 'react-bootstrap';
 
 function App() {
   return (
-
-    <Navbar sticky="top" expand="lg" className="bg-body-secondary shadow-sm">
-      <Container fluid>
-        <Navbar.Brand href="/index.html">
-          <img 
-            src="/public/images/20240109_Martin.png" 
-            alt="Logo" 
-            className="d-inline-block align-top me-1"
-            height= "32"
-            width= "32"
-          ></img>
-          <strong>CaringCloak</strong>
-        </Navbar.Brand>
-        <Navbar.Toggle aria-controls="basic-navbar-nav" />
-        <Navbar.Collapse id="basic-navbar-nav">
-          <Nav className="me-auto">
-            <Nav.Link href="/index.html">Startseite</Nav.Link>
-            <Nav.Link href="/public/pages/registrierung.html">Registrierung</Nav.Link>
-            <Nav.Link href="/public/pages/faq.html">FAQ</Nav.Link>
-            <Nav.Link href="/public/pages/kontakt.html">Kontakt</Nav.Link>
-          </Nav>
-        </Navbar.Collapse>
-      </Container>
-    </Navbar>
-
+    <>
+      <Header />
+      <Row>
+        <Col>
+          <Logistikauswahl />
+        </Col>
+        <Col>
+        </Col>
+      </Row>
+    </>
   );
-}
+};
 
 export default App;
--- src/main.jsx
+++ src/main.jsx
@@ -1,9 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import App from './App.jsx';
-import 'bootstrap/dist/css/bootstrap.min.css';
-import 'bootstrap/dist/js/bootstrap.bundle.js';
 import './index.css';
+import App from './App.jsx';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
